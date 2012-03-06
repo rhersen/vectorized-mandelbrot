59baaa31a4450650ade5b6d13294a666a4f89357
Rudolf Hersén <rhersen@gmail.com>
2012-03-06 20:37:21 +0100

    refactored test cases to facilitate adding calc_4

--- test-pgm.c
+++ test-pgm.c
@@ -25,65 +25,54 @@ static void _assertTrue(int actual, char *file, int line) {
     }
 }
 
+static double getElapsedSecondsSince(struct timeval* start) {
+    struct timeval now;
+    gettimeofday(&now, 0);
+    int usec = now.tv_usec - start->tv_usec;
+    int sec = now.tv_sec - start->tv_sec;
+
+    return usec * 1e-6 + sec;
+}
+
 int main(void) {
     struct timeval start;
-    struct timeval now;
-    gettimeofday(&start, 0);
 
     double iw = 2 / 2e2;
     double ih = iw;
 
-    assertEquals(calc_1(0, 1, 16, iw, ih), 3);
-    assertEquals(calc_1(7, 1, 16, iw, ih), 3);
+    void assertCalc(int x, int y, int limit, int* e) {
+        for (int i = 0; i < 4; ++i) {
+            assertEquals(calc_1(x + i, y, limit, iw, ih), e[i]);
+        }
 
-    assertEquals(calc_1(65, 64, 255, iw, ih), 7);
-    assertEquals(calc_1(66, 64, 255, iw, ih), 8);
-    assertEquals(calc_1(72, 64, 255, iw, ih), 8);
+        int r[2] = { 0 };
+
+        for (int i = 0; i < 4; i += 2) {
+            calc_2(x + i, y, limit, iw, ih, r);
+
+            for (int j = 0; j < 2; ++j) {
+                assertEquals(r[j], e[i + j]);
+            }
+        }
+    }
+
+    gettimeofday(&start, 0);
+
+    assertCalc(0, 1, 16, (int[]) {3, 3, 3, 3});
+    assertCalc(65, 64, 255, (int[]) {7, 8, 8, 8});
+    assertCalc(72, 64, 255, (int[]) {8, 8, 9, 9});
+    assertCalc(72, 78, 255, (int[]) {13, 13, 14, 15});
+    assertCalc(78, 78, 255, (int[]) {85, 0, 0, 0});
+    assertCalc(77, 77, 255, (int[]) {22, 143, 0, 0});
+    assertCalc(100, 100, 255, (int[]) {0, 0, 0, 0});
 
     assertEquals(calc_1(77, 78, 31, iw, ih), 0);
     assertEquals(calc_1(77, 78, 33, iw, ih), 32);
 
-    assertEquals(calc_1(72, 78, 255, iw, ih), 13);
-    assertEquals(calc_1(73, 78, 255, iw, ih), 13);
-    assertEquals(calc_1(74, 78, 255, iw, ih), 14);
-    assertEquals(calc_1(75, 78, 255, iw, ih), 15);
-    assertEquals(calc_1(76, 78, 255, iw, ih), 17);
-    assertEquals(calc_1(77, 78, 255, iw, ih), 32);
-    assertEquals(calc_1(78, 78, 255, iw, ih), 85);
-    assertEquals(calc_1(79, 78, 255, iw, ih), 0);
+    assertCalc(74, 78, 0x10000000, (int[]) {14, 15, 17, 32});
+    assertTrue(getElapsedSecondsSince(&start) < 0.25);
 
-    assertEquals(calc_1(78, 77, 255, iw, ih), 143);
-    assertEquals(calc_1(78, 78, 255, iw, ih), 85);
-    assertEquals(calc_1(78, 79, 255, iw, ih), 0);
-    assertEquals(calc_1(79, 78, 255, iw, ih), 0);
-    assertEquals(calc_1(79, 79, 255, iw, ih), 0);
-
-    assertEquals(calc_1(100, 100, 16, iw, ih), 0);
-
-    int r[2] = { 0 };
-
-    calc_2(0, 1, 16, iw, ih, r);
-    assertEquals(r[0], 3);
-    assertEquals(r[1], 3);
-
-    calc_2(77, 78, 0x10000000, iw, ih, r);
-    assertEquals(r[0], 32);
-    assertEquals(r[1], 85);
-    gettimeofday(&now, 0);
-    float elapsed = now.tv_usec * 1e-6 - start.tv_usec * 1e-6 + (now.tv_sec - start.tv_sec);
-    assertTrue(elapsed < 0.25);
-
-    calc_2(78, 77, 255, iw, ih, r);
-    assertEquals(r[0], 143);
-    assertEquals(r[1], 0);
-
-    calc_2(100, 100, 255, iw, ih, r);
-    assertEquals(r[0], 0);
-    assertEquals(r[1], 0);
-
-    gettimeofday(&now, 0);
-    printf("%f\n", now.tv_usec * 1e-6 - start.tv_usec * 1e-6 +
-           (now.tv_sec - start.tv_sec));
+    printf("%lf seconds\n", getElapsedSecondsSince(&start));
 
     return 0;
 }
